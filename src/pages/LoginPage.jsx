8b97992e632066e91fa7d3a5560531e90fb88de4
Pablo Curty <pablo.curty@visagio.com>
2023-01-02 14:13:41 -0300

    login page, css, login take values email password

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './LoginPage.css'
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const handleLogin = () => {
+        console.log('email', email)
+        console.log('password', password)
+        console.log('login')
+    }
+    return (
+      <div id="login">
+        <h1 className="title">Login</h1>
+        <div className="form">
+          <div className="field">
+            <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+          </div>
+          <div className='actions'>
+            <button onClick={handleLogin}>Send</button>    
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default LoginPage
