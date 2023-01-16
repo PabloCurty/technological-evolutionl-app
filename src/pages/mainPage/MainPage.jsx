121e98015cc503d82f72739c4e21d21898a3a715
Igor Marques <igormarques120o@gmail.com>
2023-01-16 09:20:00 -0300

    Feat(MainPage): Creating Dummy Experience and Modifying structure

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,69 +1,87 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import "./MainPage.css"
-import Nav from '../components/Nav'
-import Search from '../components/Search'
-import Experiences from "../components/Experiences";
+import React, { useState, useEffect, useCallback } from "react";
+import { Link } from "react-router-dom";
+import "./MainPage.css";
+import Nav from "../components/nav/Nav";
+import Experiences from "../components/experiences/Experiences";
+import Profile from "../components/profile/Profile";
+import Row from "react-bootstrap/Row";
+import { Container } from 'react-bootstrap';
+import {useNavigate} from 'react-router-dom';
 import { getExperience } from "../../services/Api";
 
 const userId = "63c17d24530026f8f814a097";
 
 const MainPage = () => {
-  const [experiences, setExperiences] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [loadingError, setLoadingError] = useState(false)
-  const loadData = async (query = '') => {
+  const [experiences, setExperiences] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [loadingError, setLoadingError] = useState(false);
+  const loadData = async (query = "") => {
     try {
-      setLoading(true)
+      let testExperiences = [
+        {
+          nameClient: "Modec",
+          nameProject: "DT",
+          nameTech: ["React"],
+          language: "Inglês",
+          _id: "1",
+          directLeaders: ["teste1", "teste2", "teste3", "teste4", "teste"],
+          period: ["2021-01-01T12:00:00Z"],
+        },
+        {
+          nameClient: "Modec",
+          nameProject: "DT",
+          nameTech: ["React"],
+          language: "Inglês",
+          _id: "2",
+          directLeaders: ["teste1", "teste2", "teste3", "teste4", "teste"],
+          period: ["2021-01-01T12:00:00Z"],
+        },
+      ];
+      setLoading(true);
       const response = await getExperience(userId);
-      setExperiences(response.data);
-      setLoading(false)
+      setExperiences(response);
+      setLoading(false);
     } catch (err) {
-      console.log(err)
-      setLoadingError(true)
+      console.log(err);
+      setLoadingError(true);
     }
-    
-  }
+  };
   useEffect(() => {
-    (async () => await loadData())()
-  }, [])
-  const handleLogout = () => {
-    console.log('logout')
-  }
-  const handleSearch = (query) => {
-    console.log('query', query)
-  }
+    (async () => await loadData())();
+  }, []);
+  const navigate = useNavigate();
+  const handleLogout = useCallback(() => navigate('/login', {replace: true}), [navigate]);
   const handleDeleteExp = (experience) => {
     console.log("delete expe", experience);
   };
   const handleNewExp = (url) => {
-    console.log('new exp', url)
-  }
+    console.log("new exp", url);
+  };
   if (loadingError) {
     return (
-      <div className='loading'>
-        Erro ao carregar os dados de Experiencia.<Link to='/login'>Voltar</Link>
+      <div className="loading">
+        Erro ao carregar os dados de Experiencia.<Link to="/login">Voltar</Link>
       </div>
-    )
+    );
   }
   if (loading) {
-    return (
-      <div className='loading'>
-        Carregando...
-      </div>
-    )
+    return <div className="loading">Carregando...</div>;
   }
   return (
     <div id="main">
       <Nav onLogout={handleLogout} />
-      <Search onSearch={handleSearch} />
-      <Experiences
-        experiences={experiences}
-        onDeleteExp={handleDeleteExp}
-        onNewExp={handleNewExp}
-      />
+      <Container>
+        <Row className="mt-5 d-flex justify-content-around">
+          <Profile />
+          <Experiences
+            experiences={experiences}
+            onDeleteExp={handleDeleteExp}
+            onNewExp={handleNewExp}
+          />
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
-export default MainPage
+export default MainPage;
